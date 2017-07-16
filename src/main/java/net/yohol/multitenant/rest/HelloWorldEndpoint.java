090d530f17e7aa9c665d5c70957307a3b22b2d4e
ghoolooloo <systemadministrator@126.com>
2017-07-16 11:19:02 +0800

    使用keycloak保护应用

--- src/main/java/net/yohol/multitenant/rest/HelloWorldEndpoint.java
+++ src/main/java/net/yohol/multitenant/rest/HelloWorldEndpoint.java
@@ -1,18 +1,32 @@
 package net.yohol.multitenant.rest;
 
+import java.util.Set;
+import java.util.Arrays;
+
 import javax.enterprise.context.ApplicationScoped;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.GET;
 import javax.ws.rs.Produces;
 
+import org.jboss.resteasy.spi.HttpRequest;
+import org.keycloak.KeycloakSecurityContext;
+
 @ApplicationScoped
 @Path("/hello")
 public class HelloWorldEndpoint {
+	@Context
+  private HttpRequest httpRequest;
 
 	@GET
 	@Produces("text/plain")
 	public Response doGet() {
-		return Response.ok("Hello from WildFly Swarm!").build();
+		KeycloakSecurityContext securityContext = (KeycloakSecurityContext) httpRequest.getAttribute(KeycloakSecurityContext.class.getName());
+		String realm = securityContext.getRealm();
+		String user = securityContext.getIdToken().getPreferredUsername();
+		Set<String> roles = securityContext.getToken().getRealmAccess().getRoles();
+
+		return Response.ok("Realm: " + realm + ", User: " + user + ", Roles: " + Arrays.toString(roles.toArray())).build();
 	}
 }
